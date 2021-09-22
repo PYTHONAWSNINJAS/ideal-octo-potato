cd772774087eed23e81ed326f93c8f1134895e80
kuharan <kuharan.bhowmik@gmail.com>
2021-09-23 00:01:48 +0530

    update merge

--- merge_files.py
+++ merge_files.py
@@ -13,7 +13,7 @@ def init():
     """
 
     lambda_write_path = "/tmp/"
-    main_s3_bucket = "pythonninjas"
+    main_s3_bucket = os.environ["main_s3_bucket"] 
     pdf_file_suffix = "_dv"
 
     session = boto3.Session()
@@ -45,25 +45,33 @@ def lambda_handler(event, context):
     ----------
     event: lambda event
     context: lambda context
-    """
-    
-    s3_client, bucket_name, lambda_write_path, pdf_file_suffix = init()
-    
-    s3_clientobj = s3_client.get_object(Bucket=bucket_name, Key='case_number/doc_pdf/control.json')
-    data = json.loads(s3_clientobj['Body'].read().decode('utf-8'))
-    exhibit_id = data['exhibit_id']
-    pdf_file_name = exhibit_id+'.pdf'
-    pdfs = []
-    for item in data['files']:
-        file = item['source'].split('/')[-1]
-        filename, file_extension = os.path.splitext(file)
-        print(f"downloading {file}")
-        s3_client.download_file(bucket_name, item['source'], lambda_write_path+file)
-        pdfs.append(lambda_write_path+file)
+    """ 
+    try:
+        s3_client, bucket_name, lambda_write_path, pdf_file_suffix = init()
+        
+        s3_clientobj = s3_client.get_object(Bucket=bucket_name, Key='case_number/doc_pdf/control.json')
+        data = json.loads(s3_clientobj['Body'].read().decode('utf-8'))
+        exhibit_id = data['exhibit_id']
+        pdf_file_name = exhibit_id+'.pdf'
+        pdfs = []
+        for item in data['files']:
+            file = item['source'].split('/')[-1]
+            filename, file_extension = os.path.splitext(file)
+            print(f"downloading {file}")
+            s3_client.download_file(bucket_name, item['source'], lambda_write_path+file)
+            pdfs.append(lambda_write_path+file)
 
-    merge_pdf(pdfs, lambda_write_path+pdf_file_name)
+        merge_pdf(pdfs, lambda_write_path+pdf_file_name)
 
-    print(f"Merged - {os.path.join(lambda_write_path, pdf_file_name)}")
-    with open(os.path.join(lambda_write_path, pdf_file_name), "rb") as data:
-        s3_client.upload_fileobj(data, bucket_name, 'case_number/doc_pdf/folder1/'+pdf_file_name)
-    
+        print(f"Merged - {os.path.join(lambda_write_path, pdf_file_name)}")
+        with open(os.path.join(lambda_write_path, pdf_file_name), "rb") as data:
+            s3_client.upload_fileobj(data, bucket_name, 'case_number/doc_pdf/'+exhibit_id+'/'+pdf_file_name)
+        return {
+                'statusCode': 200,
+                'body': "Merged"
+            }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': str(e)
+        }
