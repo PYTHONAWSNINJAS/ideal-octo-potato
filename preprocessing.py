fa36cc0cf8bd3876a732c32d86d74b43b70ceaaf
kuharan <kuharan.bhowmik@gmail.com>
2021-09-26 01:35:05 +0530

    place metadatafile in metadata bucket

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,7 @@
 import json
 import boto3
 import os
+import traceback
 
 def list_dir(prefix, local, bucket, client):
     keys = []
@@ -31,11 +32,16 @@ def place_trigger_files(bucket, files):
     folders = extract_folder_paths(files)
     for trigger_folder in folders:
         client.put_object(Body="", Bucket=bucket, Key=trigger_folder)
+
+def place_metadata_file(bucket, file):
+    client = boto3.client('s3')
+    client.put_object(Body="", Bucket=bucket, Key=file)
     
 def lambda_handler(event, context):
     try:
         s3_sub_folder=os.environ["s3_sub_folder"]
         main_s3_bucket=os.environ["main_s3_bucket"]
+        metadata_s3_bucket=os.environ["metadata_s3_bucket"]
         trigger_s3_bucket=os.environ["trigger_s3_bucket"]
         processing_type=json.loads(event['body'])['processing_type']
         session = boto3.Session()
@@ -49,15 +55,19 @@ def lambda_handler(event, context):
             s3_document_folder=json.loads(event['body'])['s3_document_folder']
             prefix=''.join([s3_folder,"/",s3_sub_folder,"/",s3_document_folder])
         
-        
         files = list_dir(prefix=prefix, local="/tmp", bucket=main_s3_bucket, client=s3_client)
+        
+        doc_metadata_file_path = prefix + '/' + s3_document_folder + "_" + str(len(files))
+        place_metadata_file(bucket=metadata_s3_bucket, file=doc_metadata_file_path)
         place_trigger_files(bucket=trigger_s3_bucket, files=files)
+        
         return {
             'statusCode': 200,
             'body': "Triggered"
         }
     except Exception as e:
+        print(traceback.format_exc())
         return {
             'statusCode': 500,
-            'body': str(e)
+            'body': str(traceback.format_exc())
         }
