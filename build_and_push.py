474bb265470229edbebe7cea369af7a0cf9df00d
kuharan <kuharan.bhowmik@gmail.com>
2021-09-29 01:12:19 +0530

    Reformat and Add Docstring

--- build_and_push.py
+++ build_and_push.py
@@ -1,25 +1,39 @@
 import os
 
-aws_account_id = "176915357459"
-docker_image_name = "idealoctopotato"
-ecr_repository_name = "idealoctopotato"
 
-print("starting docker build...")
-os.system("docker build --pull --rm -f \"Dockerfile\" -t " + docker_image_name + ":latest \".\"")
-print("done.")
+def build(account_id, image_name, repository_name):
+    """
 
-print("logging in aws ecr...")
-os.system(
-    "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin " + aws_account_id +
-    ".dkr.ecr.us-east-1.amazonaws.com")
-print("done.")
+    Parameters
+    ----------
+    account_id: AWS account ID
+    image_name: Docker image to be built and assigned a name
+    repository_name: AWS ECR repository name
+    """
 
-print("tagging...")
-os.system(
-    "docker tag " + docker_image_name + ":latest " + aws_account_id + ".dkr.ecr.us-east-1.amazonaws.com/" +
-    ecr_repository_name)
-print("done.")
+    print("starting docker build...")
+    os.system("docker build --pull --rm -f \"Dockerfile\" -t " + image_name + ":latest \".\"")
+    print("done.")
 
-print("pushing to ecr...")
-os.system("docker push " + aws_account_id + ".dkr.ecr.us-east-1.amazonaws.com/" + ecr_repository_name)
-print("done.")
+    print("logging in aws ecr...")
+    os.system(
+        "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin " + account_id +
+        ".dkr.ecr.us-east-1.amazonaws.com")
+    print("done.")
+
+    print("tagging...")
+    os.system(
+        "docker tag " + image_name + ":latest " + account_id + ".dkr.ecr.us-east-1.amazonaws.com/" +
+        repository_name)
+    print("done.")
+
+    print("pushing to ecr...")
+    os.system("docker push " + account_id + ".dkr.ecr.us-east-1.amazonaws.com/" + repository_name)
+    print("done.")
+
+
+if __name__ == "__main__":
+    aws_account_id = "176915357459"
+    docker_image_name = "idealoctopotato"
+    ecr_repository_name = "idealoctopotato"
+    build(aws_account_id, docker_image_name, ecr_repository_name)
