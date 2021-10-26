f99e8184c41209da0e9b5bd19ee12d4707bd03bf
kuharan <kuharan.bhowmik@gmail.com>
2021-10-26 23:17:31 +0530

    update resize values

--- app/main.py
+++ app/main.py
@@ -733,7 +733,7 @@ def tiff_to_pdf(file_path, lambda_write_path, pdf_file_name):
             )
             tmp_pdf_file_name = tmp_image_path.replace(".png", ".pdf")
             x, y = page.size
-            page = page.resize((int(x - x * 0.15), int(y - y * 0.15)), Image.ANTIALIAS)
+            page = page.resize((int(x - x * 0.45), int(y - y * 0.45)), Image.ANTIALIAS)
             page.save(tmp_image_path)
             _ = create_pdf(tmp_image_path, lambda_write_path, tmp_pdf_file_name)
             pdfs.append(os.path.join(lambda_write_path, tmp_pdf_file_name))
