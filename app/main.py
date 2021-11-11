5292b62c5e5bc7f810e4b2914f1bc5d5551e0ff0
kuharan <kuharan.bhowmik@gmail.com>
2021-11-12 01:42:34 +0530

    fix unused variable

--- app/main.py
+++ app/main.py
@@ -855,5 +855,5 @@ def lambda_handler(event, context):
         # This is to avoid double puts and parallel events in lambda
         try:
             s3_client.head_object(Bucket=merge_trigger_bucket, Key=merge_trigger_file)
-        except ClientError as e:
+        except ClientError as _:
             create_merge_trigger_file(s3_client, merge_trigger_bucket, merge_trigger_file)
