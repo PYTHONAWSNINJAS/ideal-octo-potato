54959bab427e0c8b27402af0e1d75ad03fa26f2a
kuharan <kuharan.bhowmik@gmail.com>
2021-11-26 23:34:45 +0530

    fix inconsistent

--- app/merge_files.py
+++ app/merge_files.py
@@ -322,3 +322,5 @@ def lambda_handler(event, context):
             lambda_write_path + s3_folder + "/doc_pdf/" + exhibit_id + "/",
             ignore_errors=True,
         )
+    
+    return None
