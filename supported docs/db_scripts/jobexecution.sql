b6ccad6af7c01a047dce6f952e908d0d4f7d2e8f
kuharan <kuharan.bhowmik@gmail.com>
2022-05-07 13:10:32 +0530

    add history table

--- supported docs/db_scripts/jobexecution.sql
+++ supported docs/db_scripts/jobexecution.sql
@@ -15,6 +15,16 @@ create table docviewer.jobexecution(
    PRIMARY KEY ( case_id )
 );
 
+create table docviewer.jobexecution_history(
+   case_id varchar(20) NOT NULL,
+   total_control_files int NOT NULL,
+   processed_control_files int NOT NULL,
+   unmerged_control_files int NOT NULL,
+   unprocessed_files_from_main int NOT NULL,
+   insert_datetime datetime,
+   last_update_datetime datetime
+);
+
 -- Examples
 insert into docviewer.jobexecution (jobexecution.case_id, jobexecution.total_control_files, jobexecution.processed_control_files, jobexecution.unmerged_control_files, jobexecution.unprocessed_files_from_main) values ("case_1",10,8,1,1);
 update docviewer.jobexecution set jobexecution.processed_triggers=jobexecution.processed_triggers+1 where jobexecution.case_id='case_1';
